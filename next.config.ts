d204c695ee9db854570a72390c1caed4ef29c037
Yandi <karyana@yandi.me>
2026-02-22 21:23:08 +0700

    feat(config): update Next.js config for improved source maps
    - Disabled server source maps and preload entries on start.
    - Updated server external packages to include ioredis.
    - Added enablePrerenderSourceMaps and set to false.
    - Set TypeScript to not ignore build errors.
    - Adjusted compiler configuration for production environment.

--- next.config.ts
+++ next.config.ts
@@ -3,22 +3,24 @@ import type { NextConfig } from "next"
 const config: NextConfig = {
   experimental: {
     turbopackFileSystemCacheForDev: true,
+    serverSourceMaps: false,
+    preloadEntriesOnStart: false,
   },
-  serverExternalPackages: ["pg", "pg-pool", "pg-native"],
+  serverExternalPackages: ["pg", "ioredis"],
+  enablePrerenderSourceMaps: false,
+  productionBrowserSourceMaps: false,
   reactCompiler: true,
   reactStrictMode: true,
-  compress: true,
-  typescript: { ignoreBuildErrors: true },
-  productionBrowserSourceMaps: false,
-  compiler: {
-    ...(process.env["APP_ENV"] === "production"
+  // cacheComponents: true,
+  typescript: { ignoreBuildErrors: false },
+  compiler:
+    process.env["APP_ENV"] === "production"
       ? {
           removeConsole: {
             exclude: ["error", "warn"],
           },
         }
-      : {}),
-  },
+      : {},
   images: {
     remotePatterns: [
       {
